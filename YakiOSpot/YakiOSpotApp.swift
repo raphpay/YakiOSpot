13875af63c99ff161f26557413295e4efe415d6b
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2021-11-11 09:27:45 +0100

    first commit

---
+++ YakiOSpot/YakiOSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  YakiOSpotApp.swift
+//  YakiOSpot
+//
+//  Created by Raphaël Payet on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct YakiOSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
