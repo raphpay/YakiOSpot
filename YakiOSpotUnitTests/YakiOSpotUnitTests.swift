d8a7e4541015fd338fb353eb54f5c7a5d24f193a
Raphaël Payet <raphaelpayet@MacBook-Pro-M1-de-Raphael.local>
2021-11-23 20:14:39 +0100

    Tests: Create unit tests #12

---
+++ YakiOSpotUnitTests/YakiOSpotUnitTests.swift
@@ -0,0 +1,24 @@
+//
+//  YakiOSpotUnitTests.swift
+//  YakiOSpotUnitTests
+//
+//  Created by Raphaël Payet on 23/11/2021.
+//
+
+import XCTest
+
+class YakiOSpotUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTarget() {
+        XCTAssertEqual(1, 1)
+    }
+
+}
