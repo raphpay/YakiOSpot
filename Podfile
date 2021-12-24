dd5c5daa2a2aac75d7beaa6cfcaf09a6ae6cafdd
Raphaël Payet <raphaelpayet@MacBook-Pro-M1-de-Raphael.local>
2021-12-24 12:07:29 +0100

    Feature: Configure project for remote notifications #19

--- Podfile
+++ Podfile
@@ -1,14 +1,21 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '10.0'
+# platform :ios, '9.0'
 
 target 'YakiOSpot' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for YakiOSpot
+
 pod 'Firebase/Analytics'
 pod 'Firebase/Auth'
 pod 'Firebase/Firestore'
 pod 'FirebaseFirestoreSwift'
+pod 'Firebase/Messaging'
+
+  target 'YakiOSpotUnitTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
 
 end
--- YakiOSpot/Support/AppDelegate.swift
+++ YakiOSpot/Support/AppDelegate.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 import Firebase
+import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+        configureFirebase()
+        configureNotificationCenter(application: application)
+        configureFirebaseMessaging()
+        return true
+    }
+}
+
+// MARK: - Private Methods
+extension AppDelegate {
+    private func configureFirebase() {
         FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
         
         if let firUser = API.User.CURRENT_USER {
             // A user is connected
@@ -23,7 +34,58 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             // No user connected
             API.User.CURRENT_USER_OBJECT = nil
         }
+    }
+    
+    private func configureNotificationCenter(application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+        let authorizationOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authorizationOptions) { granted, error in
+            print("user has granted notifications", granted)
+            print("Error is", error ?? "")
+        }
         
-        return true
+        application.registerForRemoteNotifications()
+    }
+    
+    private func configureFirebaseMessaging() {
+        Messaging.messaging().delegate = self
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // Register
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Fail to register
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("didFailToRegisterForRemoteNotificationsWithError", error)
+    }
+    
+    // Will present notifications
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // TODO: Process notification here
+        print("===== willPresent notification =====", notification)
+        completionHandler([.badge, .sound])
+    }
+    
+    // Did receive notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // TODO: Process notification here
+        print("didReceive response", response)
+        print("didReceive response.notification", response.notification)
+        completionHandler()
+    }
+}
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        let tokenDict = ["token": fcmToken]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"),
+                                        object: nil,
+                                        userInfo: tokenDict)
     }
 }
