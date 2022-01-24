cfe8fca51ce8ddbd1ce582c7514b1a1dfb54f0de
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-01-24 18:33:31 +0100

    Feature: Implement new session button and navigation #36

---
+++ YakiOSpot/View/Screens/SessionView.swift
@@ -0,0 +1,20 @@
+//
+//  SessionView.swift
+//  YakiOSpot
+//
+//  Created by Raphaël Payet on 24/01/2022.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SessionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SessionView()
+    }
+}
--- YakiOSpot/View/Screens/TabViews/HomeTabView.swift
+++ YakiOSpot/View/Screens/TabViews/HomeTabView.swift
@@ -14,71 +14,84 @@ struct HomeTabView: View {
     @State private var selectedIndex: Int = 0
     
     var body: some View {
-        VStack {
-            ZStack {
-                switch selectedIndex {
-                case 0:
-                    YakiView(appState: appState)
-                case 2:
-                    InfoView(appState: appState)
-                default:
-                    Text("Another screen")
-                }
-            }
-            
-            ZStack {
-                HStack {
-                    ForEach(0..<viewModel.icons.count, id: \.self) { number in
-                        Spacer()
-                        Button {
-                            changeTab(number)
-                        } label: {
-                            if number == 1 {
-                                Image(systemName: viewModel.icons[number])
-                                    .font(.system(size: 25, weight: .regular, design: .default))
-                                    .foregroundColor(.white)
-                                    .frame(width: 60, height: 60)
-                                    .background(Color.blue)
-                                    .cornerRadius(30)
-                                    .offset(y: -40)
-                            } else {
-                                VStack {
-                                    Image(systemName: viewModel.icons[number])
-                                        .font(.system(size: 25, weight: .regular, design: .default))
-                                        .foregroundColor(selectedIndex == number ? .blue : .secondary)
-                                        .frame(width: 60)
-                                    Text(viewModel.titles[number])
-                                        .foregroundColor(selectedIndex == number ? .blue : .secondary)
-                                }
-                            }
-                        }.padding(.top, 20)
-                        Spacer()
+        NavigationView {
+            VStack {
+                ZStack {
+                    switch selectedIndex {
+                    case 0:
+                        YakiView(appState: appState)
+                    case 2:
+                        InfoView(appState: appState)
+                    default:
+                        Text("Another screen")
                     }
                 }
-                Button {
-                    viewModel.showAlert.toggle()
-                } label: {
-                    Text(isUserPresent ? "Je m'en vais !" : "Je suis là !")
-                        .frame(width: 150, height: 55)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                
+                ZStack {
+                    HStack {
+                        ForEach(0..<viewModel.icons.count, id: \.self) { number in
+                            Spacer()
+                            Button {
+                                changeTab(number)
+                            } label: {
+                                if number == 1 {
+                                    Image(systemName: viewModel.icons[number])
+                                        .font(.system(size: 25, weight: .regular, design: .default))
+                                        .foregroundColor(.white)
+                                        .frame(width: 60, height: 60)
+                                        .background(Color.blue)
+                                        .cornerRadius(30)
+                                        .offset(y: -20)
+                                } else {
+                                    VStack {
+                                        Image(systemName: viewModel.icons[number])
+                                            .font(.system(size: 25, weight: .regular, design: .default))
+                                            .foregroundColor(selectedIndex == number ? .blue : .secondary)
+                                            .frame(width: 60)
+                                        Text(viewModel.titles[number])
+                                            .foregroundColor(selectedIndex == number ? .blue : .secondary)
+                                    }
+                                }
+                            }.padding(.top, 20)
+                            Spacer()
+                        }
+                    }
+                    VStack {
+                        NavigationLink(destination: SessionView()) {
+                            Text("Prévoir une session")
+                                .frame(width: 150, height: 55)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        Button {
+                            viewModel.showAlert.toggle()
+                        } label: {
+                            Text(isUserPresent ? "Je m'en vais !" : "Je suis là !")
+                                .frame(width: 150, height: 55)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                    }
+                    .offset(y: appState.showButton ? -150 : 0)
+                    .opacity(appState.showButton ? 1 : 0)
+                    .animation(.easeInOut(duration: 0.25), value: appState.showButton)
                 }
-                .offset(y: appState.showButton ? -100 : 0)
-                .opacity(appState.showButton ? 1 : 0)
-                .animation(.easeInOut(duration: 0.25), value: appState.showButton)
             }
-        }
-        .alert(isUserPresent ? "Tu confirme ne plus être au spot ?" : "Tu confirme être au spot ?",
-               isPresented: $viewModel.showAlert) {
-            Button {
-                viewModel.toggleUserPresence()
-            } label: {
-                Text(isUserPresent ? "Tu t'en vas déjà ?" : "Tu es vraiment au spot ?")
-            }
-            
-            Button {} label: {
-                Text(isUserPresent ? "Non, je reste !" : "Non, je ne suis pas au spot")
+            .navigationBarTitle("Acceuil")
+            .navigationBarHidden(true)
+            .alert(isUserPresent ? "Tu confirme ne plus être au spot ?" : "Tu confirme être au spot ?",
+                   isPresented: $viewModel.showAlert) {
+                Button {
+                    viewModel.toggleUserPresence()
+                } label: {
+                    Text(isUserPresent ? "Tu t'en vas déjà ?" : "Tu es vraiment au spot ?")
+                }
+                
+                Button {} label: {
+                    Text(isUserPresent ? "Non, je reste !" : "Non, je ne suis pas au spot")
+                }
             }
         }
     }
