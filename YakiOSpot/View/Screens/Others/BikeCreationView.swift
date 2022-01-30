61de1a149ee067eb249c44571282e4bd14b50f2d
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-01-30 19:18:12 +0100

    Refactor: Implement a view model #31

--- YakiOSpot/View/Screens/Others/BikeCreationView.swift
+++ YakiOSpot/View/Screens/Others/BikeCreationView.swift
@@ -9,35 +9,31 @@ import SwiftUI
 
 struct BikeCreationView: View {
     
-    @State private var image: UIImage = UIImage(named: Assets.noBike)!
-    @State private var bikeModel: String = ""
-    @State private var showSheet = false
-    @State private var shouldPresentDialog = false
-    @State private var selection: UIImagePickerController.SourceType = .camera
+    @StateObject private var viewModel = BikeCreationViewViewModel()
     private let imageSize = CGFloat(110)
     
     var body: some View {
         VStack(spacing: 16) {
-            Image(uiImage: image)
+            Image(uiImage: viewModel.image)
                 .resizable()
                 .frame(width: imageSize, height: imageSize)
                 .background(Color.black.opacity(0.2))
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             
             Button {
-                shouldPresentDialog = true
+                viewModel.shouldPresentDialog = true
             } label: {
                 Text("Ajouter une photo")
                     .font(.system(size: 17))
             }
             
-            FormTextField(placeholder: "Modèle du vélo", text: $bikeModel) {
-                pushBike()
+            FormTextField(placeholder: "Modèle du vélo", text: $viewModel.bikeModel) {
+                viewModel.pushBike()
             }
 
             
             Button {
-                pushBike()
+                viewModel.pushBike()
             } label: {
                 RoundedButton(title: "Ajouter mon vélo")
             }
@@ -46,44 +42,19 @@ struct BikeCreationView: View {
             Spacer()
         }
         .navigationTitle("Mon bike")
-        .confirmationDialog("Choisir une photo", isPresented: $shouldPresentDialog) {
+        .confirmationDialog("Choisir une photo", isPresented: $viewModel.shouldPresentDialog) {
             Button("Camera") {
-                selection = .camera
-                showSheet = true
+                self.viewModel.selection = .camera
+                self.viewModel.showSheet = true
             }
             Button("Bibliothèque") {
-                selection = .photoLibrary
-                showSheet = true
+                self.viewModel.selection = .photoLibrary
+                self.viewModel.showSheet = true
             }
             Button("Annuler", role: .cancel) {}
         }
-        .sheet(isPresented: $showSheet) {
-            ImagePicker(sourceType: selection, selectedImage: self.$image)
-        }
-    }
-    
-    func pushBike() {
-        guard bikeModel != "" else { return }
-        if image != UIImage(named: Assets.noBike) {
-            guard let currentUser = API.User.CURRENT_USER_OBJECT else { return }
-            StorageService.shared.uploadImage(image, for: currentUser.id) { downloadURL in
-                let bike = Bike(id: UUID().uuidString, model: bikeModel, photoURL: downloadURL.absoluteString)
-                API.Bike.session.pushBike(bike) {
-                    // Show alert and go back
-                } onError: { error in
-                    print(error)
-                }
-            } onError: { error in
-                print(error)
-            }
-
-        } else {
-            let bike = Bike(id: UUID().uuidString, model: bikeModel, photoURL: nil)
-            API.Bike.session.pushBike(bike) {
-                // Show alert and go back
-            } onError: { error in
-                print(error)
-            }
+        .sheet(isPresented: $viewModel.showSheet) {
+            ImagePicker(sourceType: viewModel.selection, selectedImage: $viewModel.image)
         }
     }
 }
---
+++ YakiOSpot/ViewModel/Others/BikeCreationViewViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  BikeCreationViewViewModel.swift
+//  YakiOSpot
+//
+//  Created by Raphaël Payet on 30/01/2022.
+//
+
+import Foundation
+import UIKit
+
+final class BikeCreationViewViewModel: ObservableObject {
+    @Published var image: UIImage = UIImage(named: Assets.noBike)!
+    @Published var bikeModel: String = ""
+    @Published var showSheet = false
+    @Published var shouldPresentDialog = false
+    @Published var selection: UIImagePickerController.SourceType = .camera
+    
+    func pushBike() {
+        guard bikeModel != "" else { return }
+        if image != UIImage(named: Assets.noBike) {
+            guard let currentUser = API.User.CURRENT_USER_OBJECT else { return }
+            StorageService.shared.uploadImage(image, for: currentUser.id) { downloadURL in
+                let bike = Bike(id: UUID().uuidString, model: self.bikeModel, photoURL: downloadURL.absoluteString)
+                API.Bike.session.pushBike(bike) {
+                    // Show alert and go back
+                } onError: { error in
+                    print(error)
+                }
+            } onError: { error in
+                print(error)
+            }
+
+        } else {
+            let bike = Bike(id: UUID().uuidString, model: bikeModel, photoURL: nil)
+            API.Bike.session.pushBike(bike) {
+                // Show alert and go back
+            } onError: { error in
+                print(error)
+            }
+        }
+    }
+}
