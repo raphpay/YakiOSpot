38e29e2e0f747c6c3a47f66272ec9ac86f64b096
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-02-16 19:28:15 +0100

    Feature: Implement UI for profile modification #31

--- YakiOSpot/View/Screens/Others/UserModificationView.swift
+++ YakiOSpot/View/Screens/Others/UserModificationView.swift
@@ -11,19 +11,81 @@ struct UserModificationView: View {
     
     @ObservedObject var profileState: ProfileState
     
+    private let imageSize = CGFloat(135)
+    
     var body: some View {
-        VStack {
-            // Profile Image
+        ScrollView {
+            Image(Assets.imagePlaceholder)
+                .resizable()
+                .frame(width: imageSize, height: imageSize)
+                .aspectRatio(contentMode: .fill)
+                .mask(Circle())
             
-            Text(profileState.user.mail)
-            // Pseudo
+            VStack(alignment: .center, spacing: 10) {
+                Button {
+                    //
+                } label: {
+                    Text("Modifier la photo de profil")
+                        .font(.system(size: 14))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                }
+                
+                Button {
+                    //
+                } label: {
+                    Text("Sauvegarder")
+                        .font(.system(size: 14))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                }
+            }
+
             
-            // Mail
+            FormTextField(isSecured: false, placeholder: "Pseudo", text: $profileState.user.pseudo, submitLabel: .next) {
+                // On commit action
+            }
             
-            // Password
+            ActionForm(profileState: profileState)
         }
-        .onAppear {
-            print("UserModif", profileState.user)
+        .navigationTitle("Modifier")
+    }
+}
+
+struct ActionForm: View {
+    
+    @ObservedObject var profileState: ProfileState
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button {
+                //
+            } label: {
+                Text("Je certifie être adhérent")
+                    .font(.system(size: 16))
+                    .foregroundColor(.blue)
+            }
+            Divider()
+            
+            Button {
+                //
+            } label: {
+                Text("Mot de passe oublié")
+                    .font(.system(size: 16))
+                    .foregroundColor(.blue)
+            }
+            Divider()
+            
+            Button {
+                //
+            } label: {
+                Text("Déconnexion")
+                    .font(.system(size: 16))
+                    .foregroundColor(.red)
+            }
         }
+        .frame(width: 333)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 10, style: .continuous).foregroundColor(.ui.gray).opacity(0.2))
     }
 }
